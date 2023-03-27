6d7a8e6f89fd2438b9fd78bad285399aee0a2905
matjs712 <matjs712@gmail.com>
2023-03-27 12:15:33 -0300

    add salir to admin sidebar

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -160,7 +160,16 @@
             </p>
           </a>
         </li>
-       
+        <li class="nav-item mt-3">
+          <a class="nav-link" href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+              {{ __('Salir') }}
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+          </form>
+        </li>
         
       </ul>
     </nav>
