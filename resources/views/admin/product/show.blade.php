be6da7cfcbd839a740d41c904355bfa53c1805d6
matjs712 <matjs712@gmail.com>
2023-03-29 21:09:50 -0300

    fix style modal producto

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -3,36 +3,52 @@
     <div class="col-md-6">{{ $producto->id }}</div>
 </div> --}}
 <div class="row">
-    <div class="col-md-6"><strong>Nombre:</strong></div>
-    <div class="col-md-6">{{ $producto->name }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Nombre:</strong></div>
+        <div class="col-md-6">{{ $producto->name }}</div>
+    </div>
 </div>
 <div class="row">
-    <div class="col-md-6"><strong>Categoria:</strong></div>
-    <div class="col-md-6">{{ $producto->category->name }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Categoria:</strong></div>
+        <div class="col-md-6">{{ $producto->category->name }}</div>
+    </div>
 </div>
 <div class="row">
-    <div class="col-md-6"><strong>Cantidad:</strong></div>
-    <div class="col-md-6">{{ $producto->qty }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Cantidad:</strong></div>
+        <div class="col-md-6">{{ $producto->qty }}</div>
+    </div>
 </div>
 <div class="row">
-    <div class="col-md-6"><strong>Slug:</strong></div>
-    <div class="col-md-6">{{ $producto->slug }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Slug:</strong></div>
+        <div class="col-md-6">{{ $producto->slug }}</div>
+    </div>
 </div>
 <div class="row">
-    <div class="col-md-6"><strong>Descripción:</strong></div>
-    <div class="col-md-6">{{ $producto->description }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Descripción:</strong></div>
+        <div class="col-md-6">{{ $producto->description }}</div>
+    </div>
 </div>
 <div class="row">
-    <div class="col-md-6"><strong>Descripción corta:</strong></div>
-    <div class="col-md-6">{{ $producto->small_description }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Descripción corta:</strong></div>
+        <div class="col-md-6">{{ $producto->small_description }}</div>
+    </div>
 </div>
 <div class="row">
-    <div class="col-md-6"><strong>Precio:</strong></div>
-    <div class="col-md-6">{{ $producto->original_price }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Precio:</strong></div>
+        <div class="col-md-6">{{ $producto->original_price }}</div>
+    </div>
 </div>
 <div class="row">
-    <div class="col-md-6"><strong>Precio en oferta:</strong></div>
-    <div class="col-md-6">{{ $producto->selling_price }}</div>
+    <div class="col-md-12 d-flex">
+        <div class="col-md-6"><strong>Precio en oferta:</strong></div>
+        <div class="col-md-6">{{ $producto->selling_price }}</div>
+    </div>
 </div>
 <br>
 <div class="row">
