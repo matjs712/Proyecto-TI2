1efa6de78687d90f49a95a64beaf24002b6f285d
matjs712 <matjs712@gmail.com>
2023-03-31 21:04:56 -0300

    custom login and register

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -8,7 +8,10 @@
   <title>@yield('title')</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Overpass:wght@200&family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+
   <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }}">
   <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css') }}">
   <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.css') }}">
@@ -18,10 +21,11 @@
   <script src="{{ asset('frontend/js/owl.carousel.js') }}"></script>
   <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
 
+
   <style>
     body{
       overflow-x: hidden;
-      font-family: 'Roboto Condensed';
+      font-family: 'Overpass', 'Sans serif';
     }
   </style>
 
--- resources/views/layouts/inc/frontNavbar.blade.php
+++ resources/views/layouts/inc/frontNavbar.blade.php
@@ -18,17 +18,23 @@
       @guest
         @if (Route::has('login'))
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">
+            {{-- <a class="nav-link" href="{{ route('login') }}">
+              {{ __('Login') }}
+            </a> --}}
+            <a type="button" class="nav-link show-modal" data-toggle="modal" data-target="#myModal">
               {{ __('Login') }}
             </a>
           </li>
-        @endif
-        
-        @if (Route::has('register'))
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('register') }}">
-            {{ __('Register') }}
-          </a>
+          @endif
+          
+          @if (Route::has('register'))
+          <li class="nav-item">
+            <a type="button" class="nav-link show-modal" data-toggle="modal" data-target="#myModalRegister">
+              {{ __('Register') }}
+            </a>
+            {{-- <a class="nav-link" href="{{ route('register') }}">
+              {{ __('Register') }}
+          </a> --}}
         </li>
         @endif
         @else
@@ -65,3 +71,155 @@
     </ul>
   </div>
 </nav>
+
+<div class="modal fade" style="width: 95%" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content clearfix">
+          {{-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button> --}}
+          <div class="modal-content clearfix">
+              <div class="modal-body">
+                  <div class="modal-icon">
+                      <i class="fas fa-desktop"></i>
+                  </div>
+                  <div class="container">
+                    <h3 class="title p-4 text-center">Bienvenido! <span>:)</span></h3>
+                    <form method="POST" action="{{ route('login') }}" class="text-start">
+                      @csrf
+
+                      <div class="row mb-3 d-flex flex-column">
+                          <label for="email" class="col-md-12 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                          <div class="col-md-12">
+                              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                              @error('email')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+                      <div class="row mb-3  d-flex flex-column">
+                          <label for="password" class="col-md-12 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                          <div class="col-md-12">
+                              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                              @error('password')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+                      <div class="row mb-3">
+                          <div class="col-md-12 d-flex align-items-center flex-wrap">
+                              <div class="form-check">
+                                  <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                  <label class="form-check-label" for="remember">
+                                      {{ __('Remember Me') }}
+                                  </label>
+                                </div>
+                          </div>
+                      </div>
+
+                      <div class="row mb-0">
+                          <div class="col-md-8 offset-md-4">
+                              <button type="submit" class="btn btn-primary">
+                                  {{ __('Login') }}
+                              </button>
+                          </div>
+                      </div>
+
+                  </form>
+                </div>
+              </div>
+              <div class="modal-footer">   
+                @if (Route::has('password.request'))
+                  <a class="btn btn-link" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                @endif
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+
+
+<div class="modal fade" style="width: 95%" id="myModalRegister" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content clearfix">
+          {{-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button> --}}
+          <div class="modal-content clearfix">
+              <div class="modal-body">
+                  <div class="modal-icon">
+                      <i class="fas fa-desktop"></i>
+                  </div>
+                  <div class="container">
+                    <h3 class="title p-4 text-center">Bienvenido! <span>:)</span></h3>
+                    <form method="POST" action="{{ route('register') }}" class="text-start">
+                      @csrf
+
+                      <div class="row mb-3 d-flex flex-column">
+                        <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                          <div class="col-md-12">
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                          </div>
+                      </div>
+                      <div class="row mb-3 d-flex flex-column">
+                          <label for="email" class="col-md-12 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                          <div class="col-md-12">
+                              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                              @error('email')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+                      <div class="row mb-3  d-flex flex-column">
+                          <label for="password" class="col-md-12 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                          <div class="col-md-12">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                          </div>
+                      </div>
+
+                      <div class="row mb-3  d-flex flex-column">
+                        <label for="password-confirm" class="col-md-12 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                          <div class="col-md-12">
+                           <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                          </div>
+                      </div>
+
+                      <div class="row mb-0">
+                          <div class="col-md-8 offset-md-4 p-3">
+                            <button type="submit" class="btn btn-primary">
+                              {{ __('Register') }}
+                            </button>
+                          </div>
+                      </div>
+
+                  </form>
+                </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
