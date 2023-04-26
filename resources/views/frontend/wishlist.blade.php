5d05f7d0852da7409b15627d2a10dd6412c4e9cb
matjs712 <matjs712@gmail.com>
2023-04-26 00:15:08 -0400

    seeders change

--- resources/views/frontend/wishlist.blade.php
+++ resources/views/frontend/wishlist.blade.php
@@ -20,30 +20,32 @@ Lista | {{ $sitio }}
                 @if ($wishlist->count() > 0)
                 <div class="row prod_data my-4">
                     @foreach ($wishlist as $item)
-                            <div class="col-md-2">
-                                <img width="100" src="{{ asset('assets/uploads/productos/'.$item->products->image) }}" alt="{{ $item->products->name }}">
+                            <div class="d-flex align-content-center m-2 col-md-12">
+                                <div class="col-md-2">
+                                    <img width="100" src="{{ asset('assets/uploads/productos/'.$item->products->image) }}" alt="{{ $item->products->name }}">
+                                </div>
+                                <div class="col-md-2">
+                                    <h6><b>{{ $item->products->name }}</b></h6>
+                                </div>
+                                <div class="col-md-2">
+                                    <h6><b>${{ $item->products->selling_price }}</b></h6>
+                                </div>
+                                <div class="col-md-2">
+                                    <input hidden class="prod_id" type="text" value="{{ $item->prod_id }}">
+                                    @if ($item->products->qty >= $item->prod_qty)
+                                        <h6>Disponible</h6>
+                                        <input type="text" name="qty" class="form-control qty-input text-center" hidden value="1">
+                                    @else
+                                    <h6>Fuera de stock</h6>
+                                    @endif
+                                </div>
+                                <div class="col-md-2">
+                                    <button class="btn btn-success addCartBtn"><i class="fa fa-shopping-cart"></i>Añadir al carrito</button>
+                                </div>   
+                                <div class="col-md-2">
+                                    <button class="btn btn-danger remove-wishlist-item"><i class="fa fa-trash"></i> Remover</button>
+                                </div>   
                             </div>
-                            <div class="col-md-2">
-                                <h6><b>{{ $item->products->name }}</b></h6>
-                            </div>
-                            <div class="col-md-2">
-                                <h6><b>${{ $item->products->selling_price }}</b></h6>
-                            </div>
-                            <div class="col-md-2">
-                                <input hidden class="prod_id" type="text" value="{{ $item->prod_id }}">
-                                @if ($item->products->qty >= $item->prod_qty)
-                                    <h6>Disponible</h6>
-                                    <input type="text" name="qty" class="form-control qty-input text-center" hidden value="1">
-                                @else
-                                <h6>Fuera de stock</h6>
-                                @endif
-                            </div>
-                            <div class="col-md-2">
-                                <button class="btn btn-success addCartBtn"><i class="fa fa-shopping-cart"></i>Añadir al carrito</button>
-                            </div>   
-                            <div class="col-md-2">
-                                <button class="btn btn-danger remove-wishlist-item"><i class="fa fa-trash"></i> Remover</button>
-                            </div>   
                     @endforeach
                 </div>
                 @else
