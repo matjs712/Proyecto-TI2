0a1ea28997ca62cdf8d603fcc5b3af5312539a47
Diego San Martin <diegorsk2@gmail.com>
2023-05-03 21:07:49 -0400

    view login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,6 +70,7 @@
         </div>
     </div>
 </div> --}}
+
 <div class="container">
         <div class = "design">
             <div class="pill-1 rotate-45"></div>
@@ -78,23 +79,32 @@
             <div class="pill-4 rotate-45"></div>
         </div>
         <div class="login">
-            <h3 class="title">Iniciar sesion</h3>
-            <div class="text-input">
-                <i class="fa-solid fa-user"></i>
-                <input type="email" placeholder="Correo electronico">
-            </div>
-            <div class="text-input">
-                <i class="fa-solid fa-lock"></i>
-                <input type="password" placeholder="Contraseña">
-                <i class="fa-solid fa-eye"></i>
-            </div>
-            <button class="login-btn">Iniciar Sesion</button>
-            <a href="#" class="forgot">¿Olvidaste tu contraseña?</a>
+            <form class="" action="{{ route('login') }}" method="post">
+                @csrf
+                <h3 class="title">Iniciar sesion</h3>
+                <div class="text-input">
+                    <i class="fa-solid fa-user"></i>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Correo electronico" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    
+                    @error('email')
+                        <span class="" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="text-input">
+                    <i class="fa-solid fa-lock"></i>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Contraseña">
+                    <i class="fa-solid fa-eye"></i>
+                </div>
+                <button type="submit" class="login-btn">Iniciar Sesion</button>
+                
+            </form>    
+            <a href="{{route('password.request')}}" class="forgot">¿Olvidaste tu contraseña?</a>
             <div class="create">
-                <a href="">Resgistrarse</a>
-                <i class="ri-arrow-right-fill"></i>
+                <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                <i class="fa-solid fa-arrow-right"></i>
             </div>
         </div>
     </div>  
-
 @endsection
