86815266d44dd7a31c5baa988b37d7b4e1174d70
crozasc <72705196+crozasc@users.noreply.github.com>
2023-05-09 23:06:18 -0400

    bd notificaciones

--- app/Http/Controllers/Admin/FrontendController.php
+++ app/Http/Controllers/Admin/FrontendController.php
@@ -40,6 +40,12 @@ class FrontendController extends Controller
             "chart1" =>$ingredientes,
             "chart2" =>$registroProveedores
         ];
+
         return response()->json($data);
     }
+    public function notificaciones(){
+        
+
+    }
+    
 }
