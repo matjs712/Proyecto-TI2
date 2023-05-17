31021c776e0bcaa90707392f6005f8e0e4f0aed9
Diego San Martin <diegorsk2@gmail.com>
2023-05-17 00:59:16 -0400

    sell in person

--- resources/views/admin/sell_in_person.blade.php
+++ resources/views/admin/sell_in_person.blade.php
@@ -4,7 +4,7 @@ Usuarios | {{ $sitio }}
 @endsection
 
 @section('content')
-{{-- <div class="container">
+<div class="container">
     
         <div class="row">
             <div class="col-md-7">
@@ -12,10 +12,10 @@ Usuarios | {{ $sitio }}
                     <div class="card-body">
                         <h6>Detalles</h6>
                         <hr>
-                        <div class="row checkout-form">
+                        <div id="productos-container">
                             
                         </div>
-                        <button id="escaner" class="btn btn-primary" style="width: 100%">Escanear Producto</button>
+                        <button id="btnMostrarModal" class="btn btn-primary" style="width: 100%">Escanear Producto</button>
                     </div>
                 </div>
             </div>
@@ -24,49 +24,18 @@ Usuarios | {{ $sitio }}
                 <div class="card">
                     
                         <div class="card-body">
-                            <h6>Detalles de la orden</h6>
+                            <h6>Orden</h6>
                             <hr>
-                            <table class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Producto</th>
-                                        <th>Cantidad</th>
-                                        <th>Precio</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    
-                                </tbody>
-                            </table>
+                            <div class="d-flex justify-content-between">
+                                <p>Total:</p>
+                                <p id="precio"></p>
+                            </div>
                             <hr>
                             <button style="width: 100%;" class="btn btn-primary pagoBtn">Ir al pago</button>
                         </div>
                 </div>
             </div>
         </div>
-</div> --}}
-<div class="container">
-    <div class="row">
-    <div class="col">
-        <div id="productos-container"></div>
-        <button id="btnMostrarModal" class="btn btn-primary" style="width:640px">Escanea un producto</button>
-    </div>
-    <div class="col d-flex flex-column align-items-between">
-        <div>
-            <p>Total amount</p>
-        </div>
-        <div>
-            <p>Temporary amount</p>
-            <p>Shipping</p>
-        </div>
-        <div>
-            <p>Total amount including Vat</p>
-        </div>
-        <div>
-            Go to checkout
-        </div>
-    </div>
-    </div>
 </div>
 
 
@@ -149,30 +118,34 @@ Usuarios | {{ $sitio }}
         }
 
     function actualizarListaProductos(productos) {
-            var container = $('#productos-container');
-            container.empty();
-            productos.forEach(function (producto) {
-                let card = $('<div>').addClass('card my-2').attr('id', producto.id).css('max-width', '640px');
-                let row = $('<div>').addClass('row g-0');
-                let imageCol = $('<div>').addClass('col-md-4');
-                let image = $('<img>').addClass('img-fluid rounded-start').attr('src', '<?= Storage::url("uploads/productos/' + producto.image + '") ?>').attr('alt', 'Product Image');
-                let contentCol = $('<div>').addClass('col-md-6');
-                let cardBody = $('<div>').addClass('card-body');
-                let cardTitle = $('<h5>').addClass('card-title').text(producto.name);
-                let cardText = $('<p>').addClass('card-text').text(producto.description);
-                let ctaSection = $('<div>').addClass('col-md-2 d-flex flex-column justify-content-between');
-                let price = $('<p>').addClass('mt-4 mb-0 text-end').text(producto.selling_price);
-                let deleteBtn = $('<button>').addClass('btn btn-danger mb-3 mr-3').append($('<i>').addClass('fa fa-trash'));
+        let container = $('#productos-container');
+        let precio = 0;
+        container.empty();
+        productos.forEach(function (producto) {
+            let card = $('<div>').addClass('card my-2').attr('id', producto.id).css('max-width', '640px');
+            let row = $('<div>').addClass('row g-0');
+            let imageCol = $('<div>').addClass('col-md-4');
+            let image = $('<img>').addClass('img-fluid rounded-start').attr('src', '<?= Storage::url("uploads/productos/' + producto.image + '") ?>').attr('alt', 'Product Image');
+            let contentCol = $('<div>').addClass('col-md-6');
+            let cardBody = $('<div>').addClass('card-body');
+            let cardTitle = $('<h5>').addClass('card-title').text(producto.name);
+            let cardText = $('<p>').addClass('card-text').text(producto.description);
+            let ctaSection = $('<div>').addClass('col-md-2 d-flex flex-column justify-content-between');
+            let price = $('<p>').addClass('mt-4 mb-0 text-end').text(producto.selling_price);
+            precio += parseInt(producto.selling_price);
+            let deleteBtn = $('<button>').addClass('btn btn-danger mb-3 mr-3').append($('<i>').addClass('fa fa-trash'));
 
 
-                imageCol.append(image);
-                contentCol.append(cardBody.append(cardTitle, cardText));
-                ctaSection.append(price, deleteBtn)
-                row.append(imageCol, contentCol, ctaSection);
-                card.append(row);
-                container.append(card);
-            });
-        }
+            imageCol.append(image);
+            contentCol.append(cardBody.append(cardTitle, cardText));
+            ctaSection.append(price, deleteBtn)
+            row.append(imageCol, contentCol, ctaSection);
+            card.append(row);
+            container.append(card);
+        });
+
+        $('#precio').html('$ ' + precio);
+    }
     $('#productos-container').on('click', '.btn-danger', function () {
         let productId = $(this).closest('.card').attr('id');
         console.log(productId);
