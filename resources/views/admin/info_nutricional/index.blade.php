f8a1407cab2f36aba9f6232b093d31af3aef6be2
Anatleto <118031895+Anatleto@users.noreply.github.com>
2023-05-31 11:05:31 -0400

    editar informacion nutricional

--- resources/views/admin/info_nutricional/index.blade.php
+++ resources/views/admin/info_nutricional/index.blade.php
@@ -90,7 +90,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">Detalles del producto</h5>
+                    <h5 class="modal-title" id="modalLabel">Detalles de la informacion nutricional</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
