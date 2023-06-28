3b4d5c17174e26d0b4b0f8ceaafe4f83418a4c9c
matjs712 <matjs712@gmail.com>
2023-06-27 23:07:22 -0400

    fix footer

--- resources/views/layouts/inc/footer.blade.php
+++ resources/views/layouts/inc/footer.blade.php
@@ -66,27 +66,6 @@
                 <!-- Grid column -->
 
                 <!-- Grid column -->
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 text-white">
-                    <!-- Links -->
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        Useful links
-                    </h6>
-                    <p class="text-white">
-                        <a href="#!" class="text-reset">Pricing</a>
-                    </p>
-                    <p class="text-white">
-                        <a href="#!" class="text-reset">Settings</a>
-                    </p>
-                    <p class="text-white">
-                        <a href="#!" class="text-reset">Orders</a>
-                    </p>
-                    <p class="text-white">
-                        <a href="#!" class="text-reset">Help</a>
-                    </p>
-                </div>
-                <!-- Grid column -->
-
-                <!-- Grid column -->
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4 text-white">Contact</h6>
